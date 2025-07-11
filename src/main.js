832918166ea981dad030d8227a78d43fc43cca62
junilhwang <junil.h@kakao.com>
2025-07-11 21:01:23 +0900

    feat: 샘플 커밋

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>배포 테스트</h1>
+    <h1>배포 테스트 ${new Date().toISOString()}</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
